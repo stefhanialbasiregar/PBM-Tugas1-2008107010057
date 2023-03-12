f5041ad585d10b863e4bc814f2ff3b86e4171c39
Stefhani Alba <stefhani@mhs.unsyiah.ac.id>
2023-03-12 12:12:28 +0700

    commit app

---
+++ App/app/src/main/java/com/example/userinterface/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.userinterface;
+
+import com.example.userinterface.R;
+
+//package com.example.userinterface;
+//
+//import android.os.Bundle;
+//
+//import com.google.android.material.snackbar.Snackbar;
+//
+import androidx.appcompat.app.AppCompatActivity;
+//
+import android.os.Bundle;
+import android.view.View;
+//
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.AppBarConfiguration;
+//import androidx.navigation.ui.NavigationUI;
+//
+//import com.example.userinterface.databinding.ActivityMainBinding;
+//
+//import android.view.Menu;
+//import android.view.MenuItem;
+//
+public class MainActivity extends AppCompatActivity {
+//
+////
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
